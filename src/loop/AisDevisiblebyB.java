d3532df9ee404158f236bce99da158b22d7d6d74
geno <geno@geno-IdeaPad-3-14ITL6>
2023-07-06 22:07:49 +0530

    loop

---
+++ src/loop/AisDevisiblebyB.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class AisDevisiblebyB {
+
+	public static void main(String[] args) {
+int i=65;
+int b=5;
+for(int in=1;b!=i;in++) {
+System.out.println("divisibleby6");
+b=b*in;
+	}
+
+}
+}
